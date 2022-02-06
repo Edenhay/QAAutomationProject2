b90df23a35a43a92585ea2e2c42e34dd3f95ded6
Edenhay <96523033+Edenhay@users.noreply.github.com>
2022-02-06 11:01:35 +0200

    Update Presents.java

--- src/main/java/HomeAndActions/Presents.java
+++ src/main/java/HomeAndActions/Presents.java
@@ -12,9 +12,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 
 public class Presents extends Base {
-    private static final String PAGEURL = "https://buyme.co.il/search?budget=3&category=204&modal=login&region=9";
+    private static final String PAGEURL = "https://buyme.co.il/supplier/338799?budget=2&category=359&query=&region=12";
     private static final String GOLFANDCO = "ember2947";
     private static final String GIFTCARDAMOUNT = "ember3081";
+    private static final String INPUTAMOUNT = "200";
     private static final String CHOOSE = "ember3087";
     private static WebDriver driver;
 
@@ -41,6 +42,7 @@ public class Presents extends Base {
         String pageUrl = driver.getCurrentUrl();
         Assert.assertEquals(pageUrl, PAGEURL);
         Singleton.wait.until(ExpectedConditions.elementToBeClickable(By.id(GIFTCARDAMOUNT))).click();
+        sendKeysToElement(By.id(GIFTCARDAMOUNT) ,  INPUTAMOUNT)
         Reports.TakePicture();
         Singleton.wait.until(ExpectedConditions.elementToBeClickable(By.id(GIFTCARD))).click();
         Singleton.wait.until(ExpectedConditions.elementToBeClickable(By.id(CHOOSE))).click();
